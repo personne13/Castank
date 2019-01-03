f998e556c4b747a68cfa7e06ffb71d574a69e582
personne13 <thomasdamiani13@gmail.com>
2019-01-03 16:51:15 +0100

    Added preview edition
    Able to preview edition, bug fixes

--- Assets/AddElement.cs
+++ Assets/AddElement.cs
@@ -5,18 +5,77 @@ using UnityEngine;
 public class AddElement : MonoBehaviour
 {
     public GameObject currentSelection;
+    private GameObject previewedObject;
+
+
+    private GameObject test;
+
     private int currentPlugIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        previewedObject = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        CheckCursorCollision();
+        PreviewEdition();
+        //CheckCursorCollision();
+    }
+
+    void PreviewEdition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            int layerPlug = LayerMask.NameToLayer("Plug");
+            if (hit.collider.gameObject.layer == layerPlug)
+            {
+                if(previewedObject != null)
+                {
+                    return;
+                }
+
+                Transform attachedElement = hit.collider.gameObject.transform.parent;
+                Transform plug = currentSelection.transform.GetChild(currentPlugIndex);
+                Transform plugged = hit.collider.transform;
+                Vector3 newElementPosition = plugged.position + plug.position;
+                Vector3 normalPlug = plug.forward;
+                Vector3 normalPlugged = plugged.forward;
+                
+                previewedObject = Instantiate(currentSelection, newElementPosition, attachedElement.rotation);
+
+                foreach (Rigidbody r in previewedObject.GetComponentsInChildren<Rigidbody>())
+                {
+                    r.isKinematic = true;
+                }
+                Vector3 axisRotation = Vector3.Cross(normalPlug, normalPlugged);
+                if (Vector3.Cross(normalPlug, normalPlugged) != Vector3.zero)
+                    previewedObject.transform.RotateAround(plugged.position, axisRotation, Vector3.Angle(normalPlug, normalPlugged));
+                else
+                    previewedObject.transform.RotateAround(plugged.position, Vector3.up, Vector3.Angle(normalPlug, normalPlugged));
+            }
+            else
+            {
+                //ResetPreview();
+            }
+        }
+        else
+        {
+            ResetPreview();
+        }
+    }
+
+    void ResetPreview()
+    {
+        if (previewedObject != null)
+        {
+            Object.Destroy(previewedObject);
+            previewedObject = null;
+        }
     }
 
     void CheckCursorCollision()
