fbdfecf25576b851a411f8b566341e5a12b4a436
zimmermann nicolas <zimmermann.nicolas@outlook.fr>
2019-01-29 16:03:35 +0100

    nada

---
+++ Assets/Game/genWorld.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class genWorld : MonoBehaviour {
+    int R = 80;
+    int nbTrees = 8000;
+    float xSemiLength = 150;
+    float ySemiLength = 120;
+    public GameObject[] trees = new GameObject[4];
+	// Use this for initialization
+	void Start () {
+        int k = 0;
+        int it = 0;
+        while (k < nbTrees && it < 1000000000)
+        {
+            float x = Random.Range(-xSemiLength, xSemiLength);
+            float y = Random.Range(-ySemiLength, ySemiLength);
+            Vector3 pos = new Vector3(x, 0, y);
+            if (pos.magnitude > R - 5)
+            {
+                Instantiate(trees[Random.Range(0, 4)], pos, Quaternion.identity);
+                k++;
+            }
+            it++;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
